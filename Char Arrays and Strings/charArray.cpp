d43b351141ad8eb741bb6125d2bc7693d44adc5e
Anmol Gupta <anmolg2411@gmail.com>
2024-03-23 13:36:37 +0000

    lenght

--- Char Arrays and Strings/charArray.cpp
+++ Char Arrays and Strings/charArray.cpp
@@ -3,10 +3,28 @@ using namespace std;
 
 int main(){
     char ch[10];
-    cin >> ch;
+    // cin >> ch;
+    // cout << ch <<endl;
+
+    // cout <<((int)ch[5]) <<endl; 
+
+    // if we want to accept string with space we uses getline()
+
+    cin.getline(ch,10);
+
     cout << ch <<endl;
 
-    cout <<((int)ch[5]) <<endl; 
+    // Length of the string
+    int count = 0;
+    int i = 0;
+    while(ch[i] != '\0'){
+        count++;
+        i++;
+    }
+    cout << "length = " << count <<endl;
+
+    // Reverse string
+    
 
     return 0;
 }
