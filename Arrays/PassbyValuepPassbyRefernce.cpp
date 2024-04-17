65bbd32daf2fb06232d1f0134591e4114274606a
Anmol Gupta <anmolg2411@gmail.com>
2024-04-17 04:55:05 +0000

    again

--- Arrays/PassbyValuepPassbyRefernce.cpp
+++ Arrays/PassbyValuepPassbyRefernce.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void incrementBy1(int n ){
+void incrementBy1(int &n ){
     n++;
     cout << "n : " << n << endl;
 }
@@ -12,13 +12,13 @@ int main(){
     cin >> n;
 
     // pass by value
-    cout << "pass by value" << endl;
-    incrementBy1(n);
-    cout << n ;
+    // cout << "pass by value" << endl;
+    // incrementBy1(n);
+    // cout << n <<endl;
 
     // passBy refernece
-    // cout << "pass by refernece" << endl;
-    // incrementBy1(n);
-    // cout << n;
+    cout << "pass by refernece" << endl;
+    incrementBy1(n);
+    cout << n <<endl;
     return 0;
 }
--- Arrays/tempCodeRunnerFile.cpp
+++ Arrays/tempCodeRunnerFile.cpp
@@ -1,8 +1,3 @@
- // for(int i=0;i<5;i++){
-    //     cin >> arr[i] ;
-    // }
-
-    //  // print
-    // for(int i=0;i<5;i++){
-    //     cout << arr[i] <<endl;
-    // }
+cout << "pass by value" << endl;
+    // incrementBy1(n);
+    // cout << n ;
